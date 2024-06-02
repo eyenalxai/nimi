d54c5c6ec64dab9e620a8d88a0f7d2ba0cb19789
Sax Dhiut <mail@iyxeyl.me>
2024-06-03 00:18:40 +0400

    feat: add URL rewrites to next.config with content-type detection

--- next.config.js
+++ next.config.js
@@ -1,4 +1,57 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+	async rewrites() {
+		return {
+			beforeFiles: [
+				{
+					source: "/usernames",
+					destination: "/api/usernames",
+					has: [
+						{
+							type: "header",
+							key: "content-type",
+							value: "application/json"
+						}
+					]
+				},
+				{
+					source: "/usernames",
+					destination: "/api/usernames",
+					has: [
+						{
+							type: "header",
+							key: "content-type",
+							value: "text/plain"
+						}
+					]
+				},
+				{
+					source: "/fullnames",
+					destination: "/api/fullnames",
+					has: [
+						{
+							type: "header",
+							key: "content-type",
+							value: "application/json"
+						}
+					]
+				},
+				{
+					source: "/fullnames",
+					destination: "/api/fullnames",
+					has: [
+						{
+							type: "header",
+							key: "content-type",
+							value: "text/plain"
+						}
+					]
+				}
+			],
+			afterFiles: [],
+			fallback: []
+		}
+	}
+}
 
 module.exports = nextConfig
