f176c9389d64fc934882f60c028ea570b8f2f9e9
Sax Dhiut <mail@iyxeyl.me>
2024-06-03 00:49:54 +0400

    debug: log host header in UsernamesPage function

--- app/usernames/page.tsx
+++ app/usernames/page.tsx
@@ -1,10 +1,12 @@
 import { DataFetch } from "@/components/data-fetch"
 import { getUsernames } from "@/lib/fetch"
 import { HydrationBoundary, QueryClient, dehydrate } from "@tanstack/react-query"
+import { headers } from "next/headers"
 
 export default async function UsernamesPage() {
 	const queryClient = new QueryClient()
-
+	const headersList = headers()
+	console.log("host", headersList.get("host"))
 	await queryClient.prefetchQuery({
 		queryKey: ["usernames"],
 		queryFn: getUsernames
