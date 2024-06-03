18239ea5c9ab65a2a1356568fdcb14d88a933841
Sax Dhiut <mail@iyxeyl.me>
2024-06-03 12:19:07 +0400

    refactor: remove no-store cache policy from fetch requests

--- lib/fetch.ts
+++ lib/fetch.ts
@@ -1,11 +1,7 @@
 export const getUsernames = async () => {
-	return fetch("/api/usernames", {
-		cache: "no-store"
-	}).then((res) => res.json() as Promise<string[]>)
+	return fetch("/api/usernames", {}).then((res) => res.json() as Promise<string[]>)
 }
 
 export const getFullnames = async () => {
-	return fetch("/api/fullnames", {
-		cache: "no-store"
-	}).then((res) => res.json() as Promise<string[]>)
+	return fetch("/api/fullnames", {}).then((res) => res.json() as Promise<string[]>)
 }
