41e7adc321c541800a4390ab3f51f0f8d5234a5a
Sax Dhiut <mail@iyxeyl.me>
2024-12-03 17:27:27 +0400

    feat: redirect to usernames page on home load

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,5 @@
-export default function Home() {
-	return (
-		<main>
-			<h1>Hello</h1>
-		</main>
-	)
+import { redirect } from "next/navigation"
+
+export default function Index() {
+	redirect("/usernames")
 }
