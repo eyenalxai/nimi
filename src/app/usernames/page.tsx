2eca8d4d851cfa51b221a1067e2d1971ba691790
Sax Dhiut <mail@iyxeyl.me>
2024-12-03 18:04:16 +0400

    feat: add revalidation for Usernames and Fullnames pages to improve performance

--- src/app/usernames/page.tsx
+++ src/app/usernames/page.tsx
@@ -1,6 +1,8 @@
 import { NamesDisplay } from "@/components/names-display"
 import { generateUsernames } from "@/lib/generate"
 
+export const revalidate = 600
+
 export default function Usernames() {
 	return <NamesDisplay names={generateUsernames({ count: 10, min: 3 })} />
 }
